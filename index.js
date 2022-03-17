045d1b2578ba694a725c9921e56ae9bd3a147545
Tatiana Ciocca <18190041+Tacial@users.noreply.github.com>
2022-03-17 18:47:43 -0300

    update 2

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
-    $('#myModal').Modal('show');
 
+    // $('#myModal').Modal('show');
+
+    $('#startGame').click() {
+        capturaNomeJogadores();
+    };
+
+    $("td").click(function() {
+        alert('click')
+    });
 });
+
+startGame = true;
+
+function capturaNomeJogadores() {
+    var playerOne = $('input[name="txtJog1"]').val();
+    var playerTwo = $('input[name="txtJog2"]').val();
+    
+    if (playerOne == '') {
+        alert("nome necessário");
+        startGame = false;
+    };
+        
+    if (playerTwo == '') {
+        alert("nome necessário");
+        startGame = false;
+    };
+
+    if (startGame == true) {
+        $('#myModal').Modal('hide');
+        preencheNomeJogadores();
+        $('#game').show('slow');
+    };
+};
+
+function preencheNomeJogadores() {
+    var playerOne = $('input[name="txtJog1"]').val();
+    var playerTwo = $('input[name="txtJog2"]').val();
+
+    $('#playerOne').html(playerOne);
+    $('#playerTwo').html(playerTwo);
+}
